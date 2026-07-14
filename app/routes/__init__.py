1fa6a9a980a03df32d03a002dea925299e34b427
Kamsika <kamsika0401@gmail.com>
2026-07-14 20:32:42 +0530

    init.py file created

---
+++ app/routes/__init__.py
@@ -0,0 +1,17 @@
+from app.routes.auth_routes import auth_bp
+from app.routes.institution_routes import institution_bp
+from app.routes.classroom_routes import classroom_bp
+from app.routes.attendance_routes import attendance_bp
+from app.routes.study_log_routes import study_log_bp
+from app.routes.sms_log_routes import sms_log_bp
+from app.routes.student_routes import student_bp
+
+__all__ = [
+    "auth_bp",
+    "institution_bp",
+    "classroom_bp",
+    "attendance_bp",
+    "study_log_bp",
+    "sms_log_bp",
+    "student_bp",
+]
