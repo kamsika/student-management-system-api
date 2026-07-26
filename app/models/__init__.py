bb7984315545bb6660ff317e459b69d03b40bbd6
Kamsika <kamsika0401@gmail.com>
2026-07-26 07:43:30 +0530

    Add Timetable model to the API's model exports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,6 +6,7 @@ from app.models.attendance_model import Attendance
 from app.models.study_log_model import StudyLog
 from app.models.sms_log_model import SmsLog
 from app.models.billing_model import BillingRecord
+from app.models.timetable_model import Timetable
 
 __all__ = [
     "Institution",
@@ -16,4 +17,5 @@ __all__ = [
     "StudyLog",
     "SmsLog",
     "BillingRecord",
+    "Timetable",
 ]
